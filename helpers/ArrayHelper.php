affaf37ce1459e8ce5a3379cbcadaceec7ff9e55
王新龙 <86458969@qq.com>
2021-08-20 15:18:15 +0800

    ada lib composer配置

--- helpers/ArrayHelper.php
+++ helpers/ArrayHelper.php
@@ -8,4 +8,109 @@ class ArrayHelper
         echo 1;
         die;
     }
+
+    /**
+     * 从数组or对象中获取数据
+     *
+     * @param      $array   数组or对象
+     * @param      $key     指定要获取的key
+     * @param null $default 默认值or以点号分割的数组键名or回调函数
+     * @return mixed|null
+     * 例子：
+     *
+     * @author 王新龙
+     * @date   2021-08-20 15:04
+     */
+    public static function getValue($array, $key, $default = NULL)
+    {
+        if ($key instanceof \Closure) {
+            return $key($array, $default);
+        }
+
+        if (is_array($key)) {
+            $lastKey = array_pop($key);
+            foreach ($key as $keyPart) {
+                $array = static::getValue($array, $keyPart);
+            }
+            $key = $lastKey;
+        }
+
+        if (is_array($array) && (isset($array[$key]) || array_key_exists($key, $array))) {
+            return $array[$key];
+        }
+
+        if (($pos = strrpos($key, '.')) !== FALSE) {
+            $array = static::getValue($array, substr($key, 0, $pos), $default);
+            $key   = substr($key, $pos + 1);
+        }
+
+        if (is_object($array)) {
+            // this is expected to fail if the property does not exist, or __get() is not implemented
+            // it is not reliably possible to check whether a property is accessible beforehand
+            return $array->$key;
+        } elseif (is_array($array)) {
+            return (isset($array[$key]) || array_key_exists($key, $array)) ? $array[$key] : $default;
+        }
+
+        return $default;
+    }
+
+    public static function getColumn($array, $name, $keepKeys = TRUE)
+    {
+        $result = [];
+        if ($keepKeys) {
+            foreach ($array as $k => $element) {
+                $result[$k] = static::getValue($element, $name);
+            }
+        } else {
+            foreach ($array as $element) {
+                $result[] = static::getValue($element, $name);
+            }
+        }
+
+        return $result;
+    }
+
+    public static function toArray($object, $properties = [], $recursive = TRUE)
+    {
+        if (is_array($object)) {
+            if ($recursive) {
+                foreach ($object as $key => $value) {
+                    if (is_array($value) || is_object($value)) {
+                        $object[$key] = static::toArray($value, $properties, TRUE);
+                    }
+                }
+            }
+
+            return $object;
+        } elseif (is_object($object)) {
+            if (!empty($properties)) {
+                $className = get_class($object);
+                if (!empty($properties[$className])) {
+                    $result = [];
+                    foreach ($properties[$className] as $key => $name) {
+                        if (is_int($key)) {
+                            $result[$name] = $object->$name;
+                        } else {
+                            $result[$key] = static::getValue($object, $name);
+                        }
+                    }
+
+                    return $recursive ? static::toArray($result, $properties) : $result;
+                }
+            }
+            if ($object instanceof Arrayable) {
+                $result = $object->toArray([], [], $recursive);
+            } else {
+                $result = [];
+                foreach ($object as $key => $value) {
+                    $result[$key] = $value;
+                }
+            }
+
+            return $recursive ? static::toArray($result, $properties) : $result;
+        }
+
+        return [$object];
+    }
 }
